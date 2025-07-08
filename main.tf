a1abdaf194a32d00e508dfbec45b9d2ac8af237e
sindhu562001 <155700200+sindhu562001@users.noreply.github.com>
2025-07-08 09:41:00 -0500

    added GKE cluster to test sentinal policy

--- main.tf
+++ main.tf
@@ -13,6 +13,11 @@ resource "random_pet" "name" {
   length = 2
 }
 
+resource "google_container_cluster" "test" {
+  name     = "test-cluster"
+  location = "us-central1"
+}
+
 output "pet_name" {
   value = random_pet.name.id
 }
